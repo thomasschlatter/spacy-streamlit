03597e3e8136859c72fde7be4745261533964092
Haowen Jiang <howard.haowen@gmail.com>
2022-08-16 22:44:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 import streamlit as st
 
-st.markdown("# AI模型輔助語言學習 | Language Learning Assisted by AI Models")
-st.markdown(f"- 開啟左側選單可選擇語言，目前支援華語、日語和英語。 | Select a language from the sidebar. Supported languages include Mandarin, Japanese, and English.")
-st.markdown(f"- 選單自動隱藏時，點選左上角 > 符號以開啟選單。 | If the sidebar is hidden, click on the > symbol in the upper left corner to open it. ")
+st.markdown("""
+
+# AI模型輔助語言學習 
+
+## Language Learning Assisted by AI Models
+
+- 開啟左側選單可選擇語言，目前支援華語、日語和英語。
+  - Select a language from the sidebar. Supported languages include Mandarin, Japanese, and English.
+- 選單自動隱藏時，點選左上角 > 符號以開啟選單。
+  - If the sidebar is hidden, click on the > symbol in the upper left corner to open it.
+  
+""")
